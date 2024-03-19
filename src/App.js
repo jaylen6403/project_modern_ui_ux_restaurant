36c167a27bd56061c46b94bce2f2fc983d000a5b
jaylen6403 <57810109+jaylen6403@users.noreply.github.com>
2024-03-19 11:46:55 -0700

    add_routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
 
 const App = () => (
   <div>
-    <Navbar />
-    <Header />
-    <AboutUs />
-    <SpecialMenu />
-    <Chef />
-    <Intro />
-    <Laurels />
-    <Gallery />
-    <FindUs />
-    <Footer />
+    <>
+      <Router>
+        <Navbar />
+        <Header />
+        <AboutUs />
+        <SpecialMenu />
+        <Chef />
+        <Intro />
+        <Laurels />
+        <Gallery />
+        <FindUs />
+        <Footer />
+        <Routes>
+          <Route path="/SpecialMenu" element={<SpecialMenu />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   </div>
 );
 
 export default App;
+
