479291975e1d68cc9e5cf3ab41fd3b8683b5af81
jaylen6403 <57810109+jaylen6403@users.noreply.github.com>
2024-03-20 22:51:00 -0700

    update_reservation_1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu, Reservation } from './container';
 import { Navbar } from './components';
 import './App.css';
 
 const App = () => (
   <div>
     <>
+      <Navbar />
       <Router>
-        <Navbar />
-        <Header />
-        <AboutUs />
-        <SpecialMenu />
-        <Chef />
-        <Intro />
-        <Laurels />
-        <Gallery />
-        <FindUs />
-        <Footer />
         <Routes>
-          <Route path="/SpecialMenu" element={<SpecialMenu />} />
+          <Route path="/" element={<><Header /><AboutUs /><SpecialMenu /><Chef /><Intro /><Laurels /><Gallery /><FindUs /><Footer /></>} />
+          <Route path="/menu" element={<><SpecialMenu /></>} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/reservation" element={<Reservation />} />
         </Routes>
         <Footer />
       </Router>
